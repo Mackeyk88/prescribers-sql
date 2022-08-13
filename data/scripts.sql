ae4134ddd005bdafb8e8e2ebfde31a4aefd03251
Mackeyk88 <mackeyk8@winthrop.edu>
2022-08-13 09:06:05 -0500

    On The End Of Question 5

--- data/scripts.sql
+++ data/scripts.sql
@@ -38,13 +38,20 @@ GROUP BY generic_name
 ORDER BY total_cost DESC
 -- Insulin Gargine, HUM.REC.AnLOG  $104,264,066.35
 
-SELECT generic_name, ROUND((SUM(total_drug_cost/total_30_day_fill_count)/30),2) AS total_cost
-FROM drug as d1
-INNER JOIN prescription AS p1
-ON d1.drug_name = p1.drug_name
-GROUP BY generic_name
-ORDER BY total_cost DESC
--- "LEDIPASVIR/SOFOSBUVIR"  $82,386.15
+-- SELECT generic_name, ROUND((SUM(total_drug_cost/total_30_day_fill_count)/30),2) AS total_cost
+-- FROM drug as d1
+-- INNER JOIN prescription AS p1
+-- ON d1.drug_name = p1.drug_name
+-- GROUP BY generic_name
+-- ORDER BY total_cost DESC
+-- -- "LEDIPASVIR/SOFOSBUVIR"  $82,386.15
+
+-- SELECT generic_name, ROUND(SUM((total_drug_cost)/total_day_supply),2) AS total_cost
+-- FROM drug as d1
+-- INNER JOIN prescription AS p1
+-- ON d1.drug_name = p1.drug_name
+-- GROUP BY generic_name
+-- ORDER BY total_cost DESC
 
 SELECT drug_name,
 CASE WHEN opioid_drug_flag = 'Y' THEN 'opioid'
@@ -53,7 +60,40 @@ CASE WHEN opioid_drug_flag = 'Y' THEN 'opioid'
      FROM drug
 
 
+SELECT subq.drug_type, CAST(SUM(total_drug_cost) as money) AS total_cost
+FROM 
+    (SELECT drug_name,
+        CASE WHEN opioid_drug_flag = 'Y' THEN 'opioid'
+        WHEN antibiotic_drug_flag = 'Y' THEN 'antibiotic'
+        ELSE 'Neither' END AS drug_type
+        FROM drug) AS subq
+INNER JOIN prescription
+ON subq.drug_name = prescription.drug_name
+WHERE drug_type = 'opioid' OR drug_type = 'antibiotic' 
+GROUP BY subq.drug_type
+ORDER BY total_cost DESC
+-- Opioid Total Cost - "$105,080,626.37"
+-- Antibiotic Total Cost - "$38,435,121.26"
 
+SELECT state, COUNT(DISTINCT cbsa) AS number_of_cbsa
+FROM fips_county AS fc
+INNER JOIN cbsa AS cb
+ON fc.fipscounty = cb.fipscounty
+WHERE state = 'TN'
+GROUP BY state
+-- 10
+
+SELECT cbsa, SUM(population) AS total_pop
+FROM cbsa AS cb
+INNER JOIN population AS pop
+ON cb.fipscounty = pop.fipscounty
+INNER JOIN fips_county AS fc
+ON cb.fipscounty = fc.fipscounty
+WHERE state = 'TN'
+GROUP BY cbsa
+ORDER BY total_pop DESC
+-- Highest Total pop - "34980"  1830410
+-- Smallest Total pop - "34100"  116352
 
 
 
