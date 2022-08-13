9904d209ba0371813358fad9dac144710cbcdf3f
Mackeyk88 <mackeyk8@winthrop.edu>
2022-08-13 12:04:37 -0500

    On Question 7B

--- data/scripts.sql
+++ data/scripts.sql
@@ -19,7 +19,7 @@ GROUP BY specialty_description
 ORDER BY total_claims DESC
 -- Family Practice 9752347
 
-SELECT specialty_description, COUNT(opioid_drug_flag) AS total_opioid_claims
+SELECT specialty_description, SUM(total_claim_count) AS total_opioid_claims
 FROM prescription as p1
 INNER JOIN prescriber as p2
 ON p1.npi = p2.npi
@@ -28,7 +28,7 @@ ON p1.drug_name = d1.drug_name
 WHERE opioid_drug_flag = 'Y'
 GROUP BY specialty_description
 ORDER BY total_opioid_claims DESC
--- Nurse Practitioner 9551
+-- Nurse Practitioner 900
 
 SELECT generic_name, SUM(total_drug_cost) AS total_cost
 FROM drug as d1
@@ -69,7 +69,7 @@ FROM
         WHEN antibiotic_drug_flag = 'Y' THEN 'antibiotic'
         ELSE 'Neither' END AS drug_type
         FROM drug) AS subq
-INNER JOIN prescription
+LEFT JOIN prescription
 ON subq.drug_name = prescription.drug_name
 WHERE drug_type = 'opioid' OR drug_type = 'antibiotic' 
 GROUP BY subq.drug_type
@@ -97,17 +97,55 @@ ORDER BY total_pop DESC
 -- Highest Total pop - "34980"  1830410
 -- Smallest Total pop - "34100"  116352
 
-SELECT cbsa, SUM(population) AS total_pop
-FROM cbsa AS cb
-INNER JOIN population AS pop
-ON cb.fipscounty = pop.fipscounty
-INNER JOIN fips_county AS fc
-ON cb.fipscounty = fc.fipscounty
-WHERE state = 'TN'
-GROUP BY cbsa
+SELECT county, cbsa, SUM(population) AS total_pop
+FROM fips_county AS fc
+LEFT JOIN cbsa AS cb
+ON fc.fipscounty = cb.fipscounty
+LEFT JOIN population AS pop
+ON fc.fipscounty = pop.fipscounty
+WHERE TRUE
+AND cb.cbsa IS NULL AND state = 'TN' 
+GROUP BY county, cbsa
 ORDER BY total_pop DESC
+-- Sevier 95523
+
+SELECT drug_name, total_claim_count
+FROM prescription
+WHERE total_claim_count >= 3000
+                 
+SELECT p1.drug_name, p1.total_claim_count, d1.opioid_drug_flag
+FROM prescription AS p1
+INNER JOIN drug AS d1
+ON p1.drug_name = d1.drug_name
+WHERE total_claim_count >= 3000 AND opioid_drug_flag = 'Y'
+
+SELECT nppes_provider_first_name, nppes_provider_last_org_name, p1.drug_name, p1.total_claim_count, d1.opioid_drug_flag
+FROM prescription AS p1
+INNER JOIN drug AS d1
+ON p1.drug_name = d1.drug_name
+INNER JOIN prescriber AS p2
+ON p1.npi = p2.npi
+WHERE total_claim_count >= 3000 AND opioid_drug_flag = 'Y'
+
+SELECT specialty_description, npi, drug_name
+FROM prescriber AS p1
+CROSS JOIN drug AS d1
+WHERE nppes_provider_city = 'NASHVILLE' AND opioid_drug_flag = 'Y'
+
+SELECT specialty_description, npi, drug_name
+FROM prescriber AS p1
+CROSS JOIN drug AS d1
+WHERE nppes_provider_city = 'NASHVILLE' AND opioid_drug_flag = 'Y'
 
 
+
+
+
+SELECT total_claim_count, p1.npi, p2.drug_name
+FROM prescriber AS p1
+CROSS JOIN drug AS d1
+RIGHT JOIN prescription AS p2
+ON p1.npi = p2.npi
 
 
 
